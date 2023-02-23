222a2ad56fc1d4297aee23c1dedf1d9b959a897c
Federico Lupotti <flupotti@applica.dev>
2023-02-23 20:01:33 -0300

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Federico Lupotti on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
